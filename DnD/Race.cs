39b6462a62511e34416467637224122e6883ae11
Darren Ives <dazleri@tiscali.co.uk>
2016-06-15 11:43:31 +0200

    Added speed to players sheet

--- DnD/Race.cs
+++ DnD/Race.cs
@@ -8,5 +8,25 @@ namespace DnD
 {
     class Race
     {
+        public string name;
+        public int charis;
+        public int con;
+        public int dex;
+        public int str;
+        public int intel;
+        public int wis;
+        public int speed;
+
+        public Race(string name, int v1, int v2, int v3, int v4, int v5, int v6,int speed)
+        {
+            this.name = name;
+            this.str = v1;
+            this.dex = v2;
+            this.con = v3;
+            this.wis = v4;
+            this.intel = v5;
+            this.charis = v6;
+            this.speed = speed;
+        }
     }
 }
